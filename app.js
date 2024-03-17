7808c2fad949d7b12d9b155e707e106d913863c3
Chris <Christopherjorge47@icloud.com>
2024-03-17 18:50:15 -0400

    First commit: Completed first exercise

---
+++ app.js
@@ -0,0 +1,14 @@
+const person = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 30,
+    hobbies: ['music', 'movies', 'sports'],
+    address:
+    {
+        street: '50 main st',
+        city: 'Boston',
+        state: 'MA'
+    }
+}
+
+console.log(person.hobbies[1])
