c36ad8f551a808db83a2d3dc772b2836916a66b6
Chris <Christopherjorge47@icloud.com>
2024-03-17 18:56:57 -0400

    Completed second exercise

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Exercise one access the movies item inside of hobbies
 const person = {
     firstName: 'John',
     lastName: 'Doe',
@@ -12,3 +13,26 @@ const person = {
 }
 
 console.log(person.hobbies[1])
+
+// Print out second objects text
+const todos = [
+    {
+        id: 1,
+        text: 'Take out trash',
+        isCompleted: true
+    },
+
+    {
+        id: 2,
+        text: 'Meeting with boss',
+        isCompleted: true
+    },
+
+    {
+        id: 3,
+        text: 'Take out trash',
+        isCompleted: true
+    }
+];
+
+console.log(todos[1].text)
